2d1bc23f7d3481b0b134da9e5487018e9f049971
Julie <julie@julies-mbp.home>
2020-11-17 19:05:00 +0100

    New Commit

--- libft/ft_isprint.c
+++ libft/ft_isprint.c
@@ -1,27 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   error.c                                            :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: inyancat <inyancat@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jd-artoi <jd-artoi@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created  2016/11/11 16:47:32 by inyancat          #+#    #+#             */
-/*   Updated  2016/11/12 20:21:42 by inyancat         ###   ########.fr       */
+/*   Created: 2020/11/16 20:35:25 by jd-artoi          #+#    #+#             */
+/*   Updated: 2020/11/16 22:06:24 by jd-artoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "utils.h"
-#include <stdarg.h>
-extern int g_log_fd;
+#include <ctype.h>
+#include <stdio.h>
 
-void error(int code, int a, const char *pattern, ...)
+int		ft_isprint(int c)
 {
-	va_list	va;
+	if (c >= 40 && c <= 176)
+		return (1);
+	return (0);
+}
 
-	va_start(va, pattern);
-	(void)a;
-	vdprintf(g_log_fd, pattern, va);
-	dprintf(g_log_fd, "\n");
-	va_end(va);
-	exit(code);
+int		main(void)
+{
+	printf("ft : %d\nc  : %d\n", ft_isprint('j'), isprint('j'));
 }
