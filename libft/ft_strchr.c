509ba80e19a02d426294157882e04adfd9e4aea1
Julie D'artois <jd-artoi@shi-r8-p3.s19.be>
2020-11-17 10:55:33 +0100

    New Commit

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -1,18 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jd-artoi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/17 10:18:45 by jd-artoi          #+#    #+#             */
-/*   Updated: 2020/11/17 10:18:47 by jd-artoi         ###   ########.fr       */
+/*   Created: 2020/11/17 10:29:40 by jd-artoi          #+#    #+#             */
+/*   Updated: 2020/11/17 10:53:52 by jd-artoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_isalpha(int c)
+#include <stdio.h>
+#include <string.h>
+
+int		ft_strchr(const char *s, int c)
 {
-	if ((c < 'A' || c > 'z') || (c > 'Z' && c < 'a'))
-		return (0);
-	return (1);
+	char *str;
+
+	str = (char*)s;
+	while (str && (*str != c))
+		str++;
+	if (*str == c)
+		return (str);
+	return (0);
+}
+
+int		main(void)
+{
+	char *str;
+
+	str = "this is the string where you search";
+	printf("ft : %d\nc  : %s\n", ft_strchr(str, 'e'), strchr(str, 'e'));
 }
