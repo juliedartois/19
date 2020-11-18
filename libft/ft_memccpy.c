2bb7961a32451f6a518e603996e36a316883a2e4
Julie <julie@Julies-MBP.lan>
2020-11-19 00:05:06 +0100

    New Commit

--- libft/ft_memccpy.c
+++ libft/ft_memccpy.c
@@ -6,24 +6,19 @@
 /*   By: jd-artoi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 15:58:00 by jd-artoi          #+#    #+#             */
-/*   Updated: 2020/11/18 16:41:38 by jd-artoi         ###   ########.fr       */
+/*   Updated: 2020/11/18 19:12:40 by jd-artoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <string.h>
 
 void	*ft_memccpy(void *b, const void *a, int c, size_t n)
 {
-	char		*dest;
-	const char	*src;
 
-	dest = (char*)b;
-	src = (const char*)a;
-	if (a == NULL && b == NULL)
-		return (NULL);
-	while (n-- > 0 || src[n] != (unsigned char)c)
-		dest[n] = src[n];
-	if (src[n] == c)
-		return (&dest[++n]);
-	return (b);
+}
+
+int		main(void)
+{
+	memccpy();
 }
