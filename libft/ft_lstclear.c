c70b218d4e3802a0d5b2ca7f218300452de67bcc
juliedartois <julie.dartois@icloud.com>
2020-12-14 11:49:34 +0100

    New Commit

--- libft/ft_lstclear.c
+++ libft/ft_lstclear.c
@@ -5,39 +5,13 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jd-artoi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-<<<<<<< HEAD
-/*   Created: 2020/12/02 11:23:02 by jd-artoi          #+#    #+#             */
-/*   Updated: 2020/12/02 14:05:11 by jd-artoi         ###   ########.fr       */
-=======
 /*   Created: 2020/12/03 10:01:00 by jd-artoi          #+#    #+#             */
-/*   Updated: 2020/12/09 15:51:49 by jd-artoi         ###   ########.fr       */
->>>>>>> 7b3fd82596a22586018655d231f07c6519c70e27
+/*   Updated: 2020/12/14 11:47:55 by jd-artoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-<<<<<<< HEAD
-static void	ft_rec(t_list *item, void (*del)(void*))
-{
-	if (item->next)
-		ft_rec(item->next, del);
-	if (!(item->next))
-	{
-		del(item);
-		free(item);
-	}
-}
-
-void		ft_lstclear(t_list **lst, void (*del)(void*))
-{
-	t_list	*item;
-
-	item = *lst;
-	ft_rec(item, del);
-	lst = NULL;
-}
-=======
 void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
 	t_list	*a;
@@ -53,4 +27,3 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 		}
 	}
 }
->>>>>>> 7b3fd82596a22586018655d231f07c6519c70e27
--- libft/libft.h
+++ libft/libft.h
@@ -6,11 +6,7 @@
 /*   By: jd-artoi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 10:19:15 by jd-artoi          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2020/12/02 11:23:38 by jd-artoi         ###   ########.fr       */
-=======
-/*   Updated: 2020/12/07 10:29:32 by jd-artoi         ###   ########.fr       */
->>>>>>> 7b3fd82596a22586018655d231f07c6519c70e27
+/*   Updated: 2020/12/14 11:48:03 by jd-artoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,11 +76,8 @@ t_list				*ft_lstlast(t_list *lst);
 void				ft_lstadd_back(t_list **lst, t_list *new);
 void				ft_lstdelone(t_list *lst, void (*del)(void*));
 void				ft_lstclear(t_list **lst, void (*del)(void*));
-<<<<<<< HEAD
-=======
 void				ft_lstiter(t_list *lst, void (*f)(void *));
 t_list				*ft_lstmap\
 						(t_list *lst, void *(*f)(void *), void (*del)(void *));
->>>>>>> 7b3fd82596a22586018655d231f07c6519c70e27
 
 #endif
